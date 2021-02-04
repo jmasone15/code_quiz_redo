7ccbdbf33d1ec2872b790c8bcc68f04428447991
Jordan Masone <jordanmasone1@gmail.com>
2021-02-04 08:28:17 -0500

    Seperated start function into smaller functions.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,13 +4,18 @@ const questionsScreen = document.getElementById("questions");
 const endScreen = document.getElementById("end-screen");
 const timerVar = document.getElementById("timer");
 const highscorelink = document.getElementById("highscore-link");
-var seconds = 75;
-const questionTitle = document.getElementById("question-title");
 const questionBtn = document.getElementsByClassName("question-button");
-const questionText0 = document.getElementById("question0");
-const questionText1 = document.getElementById("question1");
-const questionText2 = document.getElementById("question2");
-const questionText3 = document.getElementById("question3");
+const userChoice = [];
+var seconds = 75;
+let questionIndex = 0;
+
+// Build answers array to pull from
+const answers = [];
+for (let i = 0; i < questions.length; i++) {
+    answers.push(questions[i].answer)
+}
+console.log(answers);
+
 
 //call quiz function on button click
 startBtn.onclick = startQuiz;
@@ -37,107 +42,68 @@ function startQuiz() {
             highscorelink.setAttribute("class", "hide")
         }
     }, 1000);
-
-    for (var i = 0; i < questionBtn.length; i++) {
-        questionBtn[i].addEventListener("click", function () {
-            let userChoice = this.innerHTML;
-            console.log(userChoice);
-
-            if (userChoice === questions[0].answer) {
-                alert("Corret!");         
-            } else {
-                alert("Incorrect!");
-            }
-        });
-    }
-
-
-    function questionUser(num) {
-        questionTitle.innerHTML = questions[num].title;
-        questionText0.innerHTML = questions[num].choices[0];
-        questionText1.innerHTML = questions[num].choices[1];
-        questionText2.innerHTML = questions[num].choices[2];
-        questionText3.innerHTML = questions[num].choices[3];
-    }
-    questionUser(0);
-
+    renderQuestion(questionIndex);
 }
 
+// Function that adds text to buttons and checks answer choice
+function renderQuestion(num) {
 
+    let questionTitle = document.getElementById("question-title");
+    let questionText0 = document.getElementById("question0");
+    let questionText1 = document.getElementById("question1");
+    let questionText2 = document.getElementById("question2");
+    let questionText3 = document.getElementById("question3");
 
+    questionTitle.innerHTML = questions[num].title;
+    questionText0.innerHTML = questions[num].choices[0];
+    questionText1.innerHTML = questions[num].choices[1];
+    questionText2.innerHTML = questions[num].choices[2];
+    questionText3.innerHTML = questions[num].choices[3];
 
+    questionIndex++
+    checkAnswer(num)
+}
 
-// function correctAnswer(button) {
-//     if (questionText0[button] === questions[button].answer) {
-//         alert = "Correct!";
-//     } else {
-//         alert = "Incorrect! The correct answer was " + questions[button].answer;
-//     }
-// };
+function checkAnswer(num) {
+    if (questionIndex < 10) {
+        for (var i = 0; i < questionBtn.length; i++) {
+            questionBtn[i].addEventListener("click", function () {
+                userChoice.push(this.innerHTML);
+                console.log(userChoice);
 
+                if (userChoice[num] === questions[num].answer) {
+                    alert("Correct!");
+                    renderQuestion(questionIndex);
+                } else if (userChoice[num] !== questions[num].answer) {
+                    alert("Incorrect!");
+                    seconds -= 10;
+                    renderQuestion(questionIndex);
+                }
+            });
+        }
+    } else if (questionIndex = 10) {
+        for (var i = 0; i < questionBtn.length; i++) {
+            questionBtn[i].addEventListener("click", function () {
+                userChoice.push(this.innerHTML);
+                console.log(userChoice);
 
+                if (userChoice[num] === questions[num].answer) {
+                    alert("Correct!");
+                    endQuiz();
+                } else if (userChoice[num] !== questions[num].answer) {
+                    alert("Incorrect!");
+                    seconds -= 10;
+                    endQuiz();
+                }
+            });
+        }
+    }
+}
 
-
-// function appendQuestions(num) {
-
-//     //grabs the correct div for the question choices
-//     var choicesDiv = document.getElementById("choices");
-
-//     //creates a p tag for the button to go inside of
-//     var questionP1 = document.createElement('P')
-//     var questionP2 = document.createElement('P')
-//     var questionP3 = document.createElement('P')
-//     var questionP4 = document.createElement('P')
-
-//     //creates a button for the question choices
-//     var buttonQuestion1 = document.createElement('BUTTON');
-//     var buttonQuestion2 = document.createElement('BUTTON');
-//     var buttonQuestion3 = document.createElement('BUTTON');
-//     var buttonQuestion4 = document.createElement('BUTTON');
-
-//     //creates the text to go inside the button
-//     var buttonText1 = document.createTextNode(questions[num].choices[0]);
-//     var buttonText2 = document.createTextNode(questions[num].choices[1]);
-//     var buttonText3 = document.createTextNode(questions[num].choices[2]);
-//     var buttonText4 = document.createTextNode(questions[num].choices[3]);
-
-//     //append question title to the page
-//     questionTitle.innerHTML = questions[num].title;
-
-//     //adds text to the button
-//     buttonQuestion1.appendChild(buttonText1);
-//     buttonQuestion2.appendChild(buttonText2);
-//     buttonQuestion3.appendChild(buttonText3);
-//     buttonQuestion4.appendChild(buttonText4);
-
-//     //adds a class to each button
-//     buttonQuestion1.setAttribute("class", "button-choice1");
-//     buttonQuestion2.setAttribute("class", "button-choice2");
-//     buttonQuestion3.setAttribute("class", "button-choice3");
-//     buttonQuestion4.setAttribute("class", "button-choice4");
-
-//     //adds an id to each button
-//     buttonQuestion1.setAttribute("id", "choice");
-//     buttonQuestion2.setAttribute("id", "choice");
-//     buttonQuestion3.setAttribute("id", "choice");
-//     buttonQuestion4.setAttribute("id", "choice");
-
-
-//     //adds button to the p tag
-//     questionP1.appendChild(buttonQuestion1);
-//     questionP2.appendChild(buttonQuestion2);
-//     questionP3.appendChild(buttonQuestion3);
-//     questionP4.appendChild(buttonQuestion4);
-
-
-//     //adds p tag to the page
-//     choicesDiv.appendChild(questionP1);
-//     choicesDiv.appendChild(questionP2);
-//     choicesDiv.appendChild(questionP3);
-//     choicesDiv.appendChild(questionP4);
-
-// };
-
-
-
-
+function endQuiz() {
+    questionsScreen.setAttribute("class", "hide");
+    endScreen.removeAttribute("class");
+    endScreen.setAttribute("class", "index");
+    timerVar.setAttribute("class", "hide");
+    highscorelink.setAttribute("class", "hide");
+}
